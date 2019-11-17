3a2118846270490f2febd869a386a0fafe6b920c
nguyenhanson1 <nguyenhanson1@gmail.com>
2019-11-17 11:04:19 -0800

    added atmosphere

---
+++ Source/UGJ2019/Public/GravObject.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GravObject.generated.h"
+
+class UStaticMeshComponent;
+class USphereComponent;
+
+UCLASS()
+class UGJ2019_API AGravObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGravObject();
+	UPROPERTY(EditInstanceOnly, Category = "Components")
+		float ForcePower;
+	UPROPERTY(EditInstanceOnly, Category = "Components")
+		float MovePower;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UStaticMeshComponent* MeshComp;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USphereComponent* InnerSphereComp;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USphereComponent* GravitySphereComp;
+	UFUNCTION()
+		void OverlapInnerSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& sweepResult);
+
+public:	
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
